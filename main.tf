a9ebf67e2efd452c2cbe28aeb518abe813a0686c
Eric Santos <eric@xcirel.net>
2024-03-23 14:15:23 -0300

    Changed stack

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "aws_iam_role_policy_attachment" "role-policy-attachment" {
 resource "aws_elastic_beanstalk_environment" "whizenv" {
   name                = "whizenvironment"
   application         = aws_elastic_beanstalk_application.whizapp.name
-  solution_stack_name = "64bit Amazon Linux 2 v3.4.1 running Corretto 17"
+  solution_stack_name = "64bit Amazon Linux 2 v3.4.4 running Corretto 17"
 
 setting {
     namespace = "aws:autoscaling:launchconfiguration"
